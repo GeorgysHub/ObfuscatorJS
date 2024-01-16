bed2dd4c9243e524214f45b32ad332b3944979ef
Gosha <gosha.balashov.1000@gmail.com>
2024-01-16 18:27:09 +0400

    com3

--- obfuscator/build.sh
+++ obfuscator/build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# Останавливаем скрипт при возникновении ошибки
 set -e
-
-# Определите имя вашего Docker-образа
 IMAGE_NAME="myapp"
-
 echo "Building Docker image: $IMAGE_NAME"
-
-# Сборка Docker-образа с использованием sudo
 sudo docker build -t $IMAGE_NAME .
-
 echo "Build completed successfully"
